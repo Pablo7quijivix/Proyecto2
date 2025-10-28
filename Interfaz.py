f7aaf0851eb5b5e013fc06306bc68f433a051af6
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-10-28 09:08:59 -0600

    [1.1] AGREGANDO INTERFAZ PyQt6

--- Interfaz.py
+++ Interfaz.py
@@ -1,1 +1,15 @@
 print(f"Agregando archivo adicional en donde se alojara la parte grafica")
+
+import sys
+from PyQt6.QtWidgets import (QApplication, QWidget, QVBoxLayout,
+                             QLineEdit, QPushButton, QLabel, QHBoxLayout)
+from Proyecto_2 import Auditor, Usuario  # Importamos la lógica
+
+class VentanaPrincipal(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Funciones de Auditor")
+
+        self.logica_auditor= Auditor(Usuario)
+
+        self.inicializar_ui()
